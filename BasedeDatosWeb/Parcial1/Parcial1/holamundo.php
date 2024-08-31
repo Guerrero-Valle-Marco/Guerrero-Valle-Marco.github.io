b7428a70822a9e43bb4135501e9b3822ae0db90b
Guerrero-Valle-Marco <159593709+Guerrero-Valle-Marco@users.noreply.github.com>
2024-08-30 18:45:11 -0700

    Práctica#2 30/08/2024
    NO TENIALACARPETA HDOCS!!!! práctica 2 screenshot ;(

---
+++ BasedeDatosWeb/Parcial1/Parcial1/holamundo.php
@@ -0,0 +1,19 @@
+<?php
+
+    $nombre = "Marco";
+    $numero = 4;
+    $numero2 = "4";
+
+    $total = $numero + $numero2;
+
+    echo "total = " .  $total;
+
+    echo "<h1> HOLA MUNDO <h1>";
+
+    for($i=0; $<10; $i++){
+        echo $i . "<br>";
+    }
+
+    echo"<h1 style='color: red;'>Marco Antonio Guerrero Valle</h6>";
+
+?>
